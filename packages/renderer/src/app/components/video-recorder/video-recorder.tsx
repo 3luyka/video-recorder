5e4eeba5d9194b737bd86da1ae55a82e21a536da
Denys Voloshyn <3luyka@gmail.com>
2025-02-22 11:56:58 -0800

    chore: recorder save callback

--- packages/renderer/src/app/components/video-recorder/video-recorder.tsx
+++ packages/renderer/src/app/components/video-recorder/video-recorder.tsx
@@ -3,8 +3,13 @@ import { useMediaRecorder } from './_hooks/use-media-recorder'
 import { useMediaStream } from '../../providers/media-stream-provider'
 import { RecorderControls } from './recorder-controls'
 import { RecorderView } from './recorder-view'
+import { useEffect } from 'react'
 
-export const VideoRecorder = () => {
+type VideoRecorderProps = {
+  onSave: ({ video, duration }: { video: Blob; duration: number }) => void
+}
+
+export const VideoRecorder = ({ onSave }: VideoRecorderProps) => {
   const { stream, size } = useMediaStream()
 
   const { canvasRef } = useCanvasRenderer({
@@ -13,14 +18,29 @@ export const VideoRecorder = () => {
     height: size?.height,
   })
 
-  const { isRecording, startRecording, stopRecording, duration } =
-    useMediaRecorder({
-      stream,
-      canvasRef,
-    })
+  const {
+    isRecording,
+    startRecording,
+    stopRecording,
+    duration,
+    recordedChunks,
+  } = useMediaRecorder({
+    stream,
+    canvasRef,
+  })
+
+  useEffect(() => {
+    if (recordedChunks.length > 0 && !isRecording) {
+      const video = new Blob(recordedChunks, { type: 'video/webm' })
+
+      onSave({ video, duration })
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [recordedChunks])
 
   return (
-    <div className="video-recorder-root">
+    <div className="w-full max-w-4xl rounded-lg bg-white p-6 shadow-lg">
       <RecorderView
         {...{
           isRecording,
