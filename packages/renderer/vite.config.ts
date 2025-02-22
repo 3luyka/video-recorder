9bb698628d210d1ac4bb676eb229209bcfbfedd7
Denys Voloshyn <3luyka@gmail.com>
2025-02-22 09:01:24 -0800

    feat: added tailwindcss

--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // @ts-expect-error - check error later
+  plugins: [react(), tailwindcss()],
 })
